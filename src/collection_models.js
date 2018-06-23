d3122ca147504397061145615a75d77a6e1ec5ea
Coleman Kane <ckane@colemankane.org>
2018-06-23 00:41:48 -0400

    Move status to the right in the charts

--- src/collection_models.js
+++ src/collection_models.js
@@ -40,25 +40,25 @@ export var collection_tables = {
   },
   ips: {
     columns: [
-      {Header: 'Status', accessor: 'status'},
       {Header: 'IP', accessor: 'ip'},
       {Header: 'Source', accessor: 'source', sortable: false,
 	        toValue: function(x) { return x.map(function(y) { return y['name']}).join(', ')}},
       {Header: 'Created', accessor: 'created'},
       {Header: 'Last Modified', accessor: 'modified'},
+      {Header: 'Status', accessor: 'status'},
     ],
     fieldset: ['source', 'ip', 'status', 'created', 'modified'],
     collection_title: "IPs"
   },
   indicators: {
     columns: [
-      {Header: 'Status', accessor: 'status'},
       {Header: 'Value', accessor: 'value'},
       {Header: 'Type', accessor: 'type'},
       {Header: 'Source', accessor: 'source', sortable: false,
 	        toValue: function(x) { return x.map(function(y) { return y['name']}).join(', ')}},
       {Header: 'Created', accessor: 'created'},
       {Header: 'Last Modified', accessor: 'modified'},
+      {Header: 'Status', accessor: 'status'},
     ],
     fieldset: ['source', 'value', 'ind_type', 'status', 'created', 'modified'],
     collection_title: "Indicators"
